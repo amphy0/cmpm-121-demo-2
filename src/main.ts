a8f6761ddb014b4c9d2b357b9ce85c8ebb3bdcad
amphy0 <106935682+amphy0@users.noreply.github.com>
2024-10-25 04:40:04 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,62 @@ import "./style.css";
 const APP_NAME = "Sticker Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
+//creating the app title
 document.title = APP_NAME;
 const header = document.createElement("h1");
 header.innerHTML = APP_NAME;
 app.append(header);
 
+//creating the canvas
 const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
 app.append(canvas);
+
+//creating the clear button
+const clearButton = document.createElement("button");
+clearButton.textContent = "Clear Canvas";
+app.appendChild(clearButton);
+
+let isDrawing = false;
+let x = 0;
+let y = 0;
+const context = canvas.getContext("2d");
+//event listener for when click starts
+canvas.addEventListener("mousedown", (e) => {
+    x = e.offsetX;
+    y = e.offsetY;
+    isDrawing = true;
+});
+//event listener for how mouse moves
+canvas.addEventListener("mousemove", (e) => {
+    if (isDrawing) {
+        drawLine(context, x, y, e.offsetX, e.offsetY);
+        x = e.offsetX;
+        y = e.offsetY;
+    }
+});
+//event listener for when click stops
+window.addEventListener("mouseup", (e) => {
+    if (isDrawing) {
+        drawLine(context, x, y, e.offsetX, e.offsetY);
+        x = 0;
+        y = 0;
+        isDrawing = false;
+    }
+});
+//function to draw the line
+function drawLine(context, x1, y1, x2, y2) {
+    context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.closePath();
+}
+
+//clear the canvas
+clearButton.addEventListener("click", () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+});
